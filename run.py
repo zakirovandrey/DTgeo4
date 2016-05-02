0244d0d6dfbda13b437d93d5f3ec4930b110e5b0
s3_zakirov <zakirov@D.plasma>
2016-05-02 23:56:46 +0300

    fix tstop

--- run.py
+++ run.py
@@ -84,7 +84,7 @@ SS.BoxMa, SS.BoxPa = SS.srcXa-5.1*dy, SS.srcXa+5.1*dy;
 SS.BoxMv, SS.BoxPv = SS.srcXv-5.1*dz, SS.srcXv+5.1*dz; 
 SS.sphR = 10*dz; SS.BoxMs, SS.BoxPs = SS.srcXs-SS.sphR-5*dx, SS.srcXs+SS.sphR+5*dx;
 boxDiagLength=sqrt((SS.BoxPs-SS.BoxMs)**2+(SS.BoxPa-SS.BoxMa)**2+(SS.BoxMv-SS.BoxPv)**2)
-SS.tStop = 0#boxDiagLength/2/min(SM.Vp,0.0001+SM.Vs)+8/(pi*SS.F0)+10*dt # 5000*dt; # ((BoxPs-BoxMs)+(BoxPa-BoxMa)+(BoxMv-BoxPv))/c+2*M_PI/Omega;
+SS.tStop = boxDiagLength/2/min(SM.Vp,0.0001+SM.Vs)+8/(pi*SS.F0)+10*dt # 5000*dt; # ((BoxPs-BoxMs)+(BoxPa-BoxMa)+(BoxMv-BoxPv))/c+2*M_PI/Omega;
 SS.V_max = 7.0;
 SS.start = 0;
 
